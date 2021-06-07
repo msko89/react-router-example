3dd57326f1b7e1b930f03b8a8568f3342fa76d59
msko <kms89123@gmail.com>
2021-06-07 23:40:56 +0900

    Context API, useContext

---
+++ src/components/Example2.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PersonContext from '../contexts/PersonContext';
+
+export default class Example2 extends React.Component {
+  // static contextType = PersonContext;
+
+  return() {
+    const persons = this.context;
+
+    return (
+      <ul>
+        {persons.map((person) => (
+          <li>{person.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+Example2.contextType = PersonContext;
